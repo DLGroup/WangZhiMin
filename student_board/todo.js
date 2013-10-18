7d44da8237657ef1f526a46c78c2e64510a4febd
teddy1004 <ivan.wang1004@gmail.com>
2013-10-18 16:14:16 +0800

    using AngularJS

---
+++ student_board/todo.js
@@ -0,0 +1,30 @@
+function TodoCtrl($scope) {
+
+    $scope.todos = [
+        {text: "Learn AngularJS", done: false},
+        {text: "Build an app", done: false}
+    ];
+
+    $scope.getTotalTodos = function() {
+        return $scope.todos.length;
+    }
+
+    $scope.remaining = function() {
+        var count = 0;
+        angular.forEach($scope.todos, function(todo){
+           count += todo.done ? 0:1;
+
+        });
+        return count
+    }
+
+    $scope.clearComplete = function() {
+        $scope.todos = _.filter($scope.todos, function(todo){
+            return !todo.done;
+        });
+    }
+    $scope.addTodo = function() {
+        $scope.todos.push({text: $scope.formTodoText, done: false});
+        $scope.formTodoText = "";
+    }
+}
